8546db4532c38f0c8fda7de91bc7de6be82188a7
gaylen <gaylen@moonjoy.net>
2016-03-24 09:08:33 +0800

    添加新项目

---
+++ changewallpaper/src/main/java/com/cc/gaylen/changewallpaper/ui/FourthFragment.java
@@ -0,0 +1,21 @@
+package com.cc.gaylen.changewallpaper.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cc.gaylen.changewallpaper.R;
+
+public class FourthFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        View view = inflater.inflate(R.layout.fourthfragment, null);
+        return view;
+    }
+
+}
---
+++ changewallpaper/src/main/java/com/cc/gaylen/changewallpaper/ui/ThirdFragment.java
@@ -0,0 +1,22 @@
+package com.cc.gaylen.changewallpaper.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cc.gaylen.changewallpaper.R;
+
+public class ThirdFragment extends Fragment {
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view=inflater.inflate(R.layout.thirdfragment, null);
+		return view;
+	}
+
+}
